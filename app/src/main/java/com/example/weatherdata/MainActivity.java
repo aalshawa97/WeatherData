bb810e6756bcfe12751a253521ed80f5c3cc8c8f
aalshawa97 <abdullahalshawa@gmail.com>
2022-03-08 16:05:21 -0800

    Weather Data

--- app/src/main/java/com/example/weatherdata/MainActivity.java
+++ app/src/main/java/com/example/weatherdata/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.weatherdata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_cityID, btn_getWeahterByID, getBtn_getWeahterByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
     }
 }
