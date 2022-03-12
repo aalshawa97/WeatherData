0f39f50a33334f505ed16433df4a167856b7ba8c
aalshawa97 <abdullahalshawa@gmail.com>
2022-03-11 17:54:21 -0800

    Weather data functional!

--- app/src/main/java/com/example/weatherdata/MainActivity.java
+++ app/src/main/java/com/example/weatherdata/MainActivity.java
@@ -1,21 +1,128 @@
 package com.example.weatherdata;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    Button btn_cityID, btn_getWeahterByID, getBtn_getWeahterByName;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    Button btn_cityID, btn_WeatherByID, btn_getWeatherByName;
     EditText et_dataInput;
-    ListView lv_weatherReport;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    ListView lv_weatherReports;
 
-        Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        //-----Assign the value to each control on the layout------//
+
+        btn_cityID = findViewById( R.id.btn_getCityID );
+        btn_WeatherByID = findViewById( R.id.btn_getWeatherByCityID );
+        btn_getWeatherByName = findViewById( R.id.btn_getWeatherByCityName );
+
+        et_dataInput = findViewById( R.id.et_dataInput );
+        lv_weatherReports = findViewById( R.id.lv_weathersReports );
+
+        final WeatherDataService weatherDataService = new WeatherDataService( MainActivity.this );
+
+        //-----Click listenners for each button.------//
+
+        btn_cityID.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+
+                weatherDataService.getCityID( et_dataInput.getText().toString(), new WeatherDataService.VolleyResponseListener()
+                {
+                    @Override
+                    public void onError( String message )
+                    {
+                        Toast.makeText( MainActivity.this, "Something went wrong ",
+                                Toast.LENGTH_SHORT ).show();
+                    }
+
+                    @Override
+                    public void onResponse( String cityID )
+                    {
+                        Toast.makeText( MainActivity.this, "Returned an ID of: " + cityID,
+                                Toast.LENGTH_LONG ).show();
+                    }
+                } );
+            }
+        } );
+
+        btn_WeatherByID.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                weatherDataService.getCityForecastByID( et_dataInput.getText().toString(), new WeatherDataService.ForeCastByIDResponse()
+                {
+                    @Override
+                    public void onError( String message )
+                    {
+                        Toast.makeText( MainActivity.this, "Something went wrong ",
+                                Toast.LENGTH_SHORT ).show();
+
+                    }
+
+                    @Override
+                    public void onResponse( List<WeatherReportModel> weatherReportModels )
+                    {
+
+                        ArrayAdapter arrayAdapter = new ArrayAdapter( MainActivity.this,android.R.layout.simple_expandable_list_item_1 , weatherReportModels);
+
+                        lv_weatherReports.setAdapter( arrayAdapter );
+
+                    }
+                } );
+            }
+        } );
+
+
+        btn_getWeatherByName.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                weatherDataService.getCityForecastByName( et_dataInput.getText().toString(), new WeatherDataService.GetCityForecastByNameCallBack()
+                {
+
+                    @Override
+                    public void Error( String message )
+                    {
+
+                        Toast.makeText( MainActivity.this, "Something went wrong ",
+                                Toast.LENGTH_SHORT ).show();
+                    }
+
+                    @Override
+                    public void onResponse( List<WeatherReportModel> weatherReportModels )
+                    {
+
+                        ArrayAdapter arrayAdapter = new ArrayAdapter( MainActivity.this,android.R.layout.simple_list_item_1 , weatherReportModels);
+
+                        lv_weatherReports.setAdapter( arrayAdapter );
+
+                    }
+                } );
+            }
+        } );
+
     }
+
 }
